aadd1dec2a0098e91e1feb7a6b93f39d4021338e
sd950216 <meen79508@gmail.com>
2023-05-20 22:54:56 +0300

    It's Showtime

--- app/Http/Controllers/StudentsCoursesController.php
+++ app/Http/Controllers/StudentsCoursesController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Departments;
 use App\Models\StudentCourses;
 use App\Models\Courses;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
